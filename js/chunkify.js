edc561dafaf0d77cabf94e7c1a06dd85d2cd03a5
devsleonardo <usr.leonardo@gmail.com>
2021-11-24 17:20:19 -0300

    2.0

--- js/chunkify.js
+++ js/chunkify.js
@@ -1,4 +1,14 @@
-const chunkify = (inputArray, perChunk) => {};
+const chunkify = (inputArray, perChunk) => {
+  let newArray = [];
+  let n = inputArray;
+  let nLength = inputArray.length;
+  let p = perChunk;
+
+  for (let i = 0; i < nLength; i++) {
+    newArray.push(n.slice(i, (i += p)));
+  }
+  return newArray;
+};
 
 console.log("----------------------------");
 console.log("Chunks de 1 items", chunkify([1, 2, 3, 4], 1));
