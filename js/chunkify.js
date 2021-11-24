aea3de26461a4837a782b6493f0b42264120b90b
devsleonardo <usr.leonardo@gmail.com>
2021-11-24 14:36:35 -0300

    1.0

---
+++ js/chunkify.js
@@ -0,0 +1,10 @@
+const chunkify = (inputArray, perChunk) => {};
+
+console.log("----------------------------");
+console.log("Chunks de 1 items", chunkify([1, 2, 3, 4], 1));
+console.log("----------------------------");
+console.log("Chunks de 2 items", chunkify([1, 2, 3, 4, 5, 6, 7], 2));
+console.log("----------------------------");
+console.log("Chunks de 3 items", chunkify([1, 2, 3, 4, 5, 6, 7], 3));
+console.log("----------------------------");
+console.log("Chunks de 4 items", chunkify([1, 2, 3], 4));
